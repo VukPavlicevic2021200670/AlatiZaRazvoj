d3d2798fcb9507c51fa05af8e9bb499ea470036f
Vuk 2 <vuk2@MacBook-Pro.local>
2026-09-10 02:24:43 +0200

    Test promene

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     # Očitavamo promenljivu iz okruženja (kasnije ćemo je podesiti u Kubernetesu)
-    message = os.getenv('MESSAGE', 'Zdravo svete!')
+    message = os.getenv('MESSAGE', 'Zdravo svete, ovo je test!')
     return jsonify({
         "message": message,
         "version": "1.0",
